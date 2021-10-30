f8038b26705c65951f75330113a73eae35eae37e
nattensleplubo <nathan.gobert2002@gmail.com>
2021-10-30 19:33:25 +0200

    Auto commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,27 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 20:07:12 by ngobert           #+#    #+#             */
-/*   Updated: 2021/10/30 18:23:29 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/10/30 19:33:13 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 
-char	*get_next_line(int fd)
-{
-	fd = open("42", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return ("raté");
-	printf("%d\n", fd);
-	write(3, "caca\ngetnextline\nvivelecovid", 28);
-	close(fd);
-	return ("covide");
-}
+// char	*get_next_line(int fd)
+// {
+
+// }
 
 int	main(void)
 {
-	get_next_line(1);
+	int	fd;
+	
+	fd = open("42", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (1);
+	printf("%d\n", fd);
+	write(3, "caca\ngetnextline\nvivelecovid", 28);
+	close(fd);
+	return (0);
 }
