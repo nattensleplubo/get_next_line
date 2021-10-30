a9f56374ebd6749548c52b304323a245dc15357b
nattensleplubo <nathan.gobert2002@gmail.com>
2021-10-30 17:36:09 +0200

    Auto commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,37 +6,14 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 20:07:12 by ngobert           #+#    #+#             */
-/*   Updated: 2021/10/26 22:34:16 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/10/27 14:26:01 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 
-//char	*get_next_line(int fd)
-//{
-//	
-//}
-
-void	ft_putstr(char *str)
+char	*get_next_line(int fd)
 {
-	int	i;
 	
-	i = 0;
-	while (str[i])
-	{
-		write(1, str, 1);
-		i++;
-	}
-}
-
-int	main(void)
-{
-	int	fd;
-	int	ret;
-	char	buf[4096 + 1];
-	
-	ret = read(42, buf, 4096);
-	buf[ret] = '\0';
-	printf("%s", ret);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,2 +1,1 @@
 #include "get_next_line.h"
-
