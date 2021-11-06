468fd249c0412ea31748bbc62e608df41b7dd8f6
nattensleplubo <nathan.gobert2002@gmail.com>
2021-11-06 20:29:37 +0100

    Auto commit

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,12 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 17:01:52 by ngobert           #+#    #+#             */
-/*   Updated: 2021/11/06 17:20:01 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/11/06 20:28:36 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 32
 
 # include <stdio.h>
 # include <unistd.h>
@@ -23,5 +22,9 @@
 char	*get_next_line(int fd);
 size_t	ft_strlen(char *str);
 char	*ft_strdup(const char *s1);
+
+#ifndef BUFFER_SIZE
+#define	BUFFER_SIZE 32
+#endif
 
 #endif
