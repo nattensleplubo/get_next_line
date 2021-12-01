89f0d420fead7ff233e625207a58e1adbbcefcb6
Nathan GOBERT <ngobert@e1r11p22.42.fr>
2021-12-01 17:17:00 +0100

    coucou

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 11:39:23 by ngobert           #+#    #+#             */
-/*   Updated: 2021/12/01 15:11:42 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/12/01 16:25:59 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ char	*get_next_line(int fd)
 {
 	char		*buffer;			// Va avoir un BUFFER_SIZE de char
 	// static char	*statique = NULL;	// Va etre utilise quand on rappel la fonction
-	char		*line;				// String du debut de la ligne jusqu'au \n qui va etre return !!
-	int			ret_val;			// Pour la return value du read
+	//char		*line;				// String du debut de la ligne jusqu'au \n qui va etre return !!
+	size_t			ret_val;			// Pour la return value du read
 
 	ret_val = 1;
 	if (fd < 0 || BUFFER_SIZE <= 0) // Check si fd et buffer_size sont bon
@@ -42,18 +42,20 @@ char	*get_next_line(int fd)
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1)); // Malloc un buffer
 	if (!buffer)
 		return (NULL);
-	while (check_eol(buffer) && ret_val != 0) // tant que ya pas de \n
-	{
-		printf("%d", ret_val);
-		ret_val = read(fd, buffer, BUFFER_SIZE);
-		printf("%s", buffer);
-		printf("%d", ret_val);
-		buffer[ret_val] = '\0';
-		line = ft_strjoin(line, buffer);
-		printf("%s", line);
-		free(buffer);
-	}
-	return (line);
+	ret_val = read(fd, buffer, BUFFER_SIZE);
+	printf("%zu", ret_val);
+	// while (check_eol(buffer) && ret_val != 0) // tant que ya pas de \n
+	// {
+	// 	ret_val = read(fd, buffer, BUFFER_SIZE);
+	// 	printf("%zu", ret_val);
+	// 	buffer[ret_val] = '\0';
+	// 	if (ret_val == 0)
+	// 		return ("bite");
+	// }
+	char *dest;
+	char hihi[] = "caca";
+	dest = malloc(sizeof(char) * ft_strlen(hihi));
+	return (dest);
 }
 
 int	main(void)
@@ -62,10 +64,11 @@ int	main(void)
 	int		fd = open("test", O_RDONLY);
 	char	*str;
 	
-	while ((str = get_next_line(fd)) != NULL)
-	{
+	// while ((str = get_next_line(fd)) != NULL)
+	// {
+		str = get_next_line(fd); // a enlever
 		printf("%s", str);
 		i++;
 		free(str);
-	}
+	// }
 }
